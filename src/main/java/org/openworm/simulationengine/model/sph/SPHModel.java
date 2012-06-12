02b2ff603a22f46dc11b8d4be731a8ae9f8b7c6e
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-06-12 17:57:58 +0200

    Added Root Element annotation

--- src/main/java/org/openworm/simulationengine/model/sph/SPHModel.java
+++ src/main/java/org/openworm/simulationengine/model/sph/SPHModel.java
@@ -10,10 +10,12 @@ package org.openworm.simulationengine.model.sph;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -45,6 +47,7 @@ import javax.xml.bind.annotation.XmlType;
     "particles",
     "cells"
 })
+@XmlRootElement
 public class SPHModel {
 
     @XmlElement(required = true)
