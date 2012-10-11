7ba75ff90da576c342c316024d3a0ef2e93063d4
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-10-11 14:34:40 +0100

    ModelProvider added. Not tested yet.

---
+++ src/main/java/org/openworm/simulationengine/model/sph/services/SPHModelProvider.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.openworm.simulationengine.model.sph.services;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.openworm.simulationengine.core.model.IModel;
+import org.openworm.simulationengine.core.model.IModelProvider;
+import org.openworm.simulationengine.model.sph.SPHModel;
+import org.openworm.simulationengine.model.sph.x.SPHModelX;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author matteocantarelli
+ *
+ */
+@Service
+public class SPHModelProvider implements IModelProvider {
+
+	/* (non-Javadoc)
+	 * @see org.openworm.simulationengine.core.model.IModelProvider#readModel(java.lang.String)
+	 */
+	public List<IModel> readModel(URL url) 
+	{
+		JAXBContext context;
+		
+		List<IModel> sphModels = new ArrayList<IModel>();
+		try 
+		{
+			context = JAXBContext.newInstance(SPHModel.class);
+			Unmarshaller um = context.createUnmarshaller();
+			SPHModelX sphModel = (SPHModelX) um.unmarshal(url);
+			sphModels.add(sphModel);
+		} 
+		catch (JAXBException e1) 
+		{
+			e1.printStackTrace();
+		}
+		return sphModels;
+	}
+
+}
