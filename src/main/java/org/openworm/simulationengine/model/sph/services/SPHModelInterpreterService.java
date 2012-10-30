165d08ad5d133b6be6785948204ef290d273c3ab
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-10-30 15:08:55 +0000

    Implemented interface to convert to visualisation model

--- src/main/java/org/openworm/simulationengine/model/sph/services/SPHModelInterpreterService.java
+++ src/main/java/org/openworm/simulationengine/model/sph/services/SPHModelInterpreterService.java
@@ -13,37 +13,82 @@ import javax.xml.bind.Unmarshaller;
 
 import org.openworm.simulationengine.core.model.IModel;
 import org.openworm.simulationengine.core.model.IModelInterpreter;
+import org.openworm.simulationengine.core.visualisation.model.AGeometry;
+import org.openworm.simulationengine.core.visualisation.model.Entity;
+import org.openworm.simulationengine.core.visualisation.model.Particle;
+import org.openworm.simulationengine.core.visualisation.model.Point;
+import org.openworm.simulationengine.core.visualisation.model.Scene;
 import org.openworm.simulationengine.model.sph.SPHModel;
+import org.openworm.simulationengine.model.sph.SPHParticle;
 import org.openworm.simulationengine.model.sph.x.SPHModelX;
 import org.springframework.stereotype.Service;
 
 /**
  * @author matteocantarelli
- *
+ * 
  */
 @Service
-public class SPHModelInterpreterService implements IModelInterpreter {
+public class SPHModelInterpreterService implements IModelInterpreter
+{
 
-	/* (non-Javadoc)
-	 * @see org.openworm.simulationengine.core.model.IModelProvider#readModel(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openworm.simulationengine.core.model.IModelProvider#readModel(java
+	 * .lang.String)
 	 */
-	public List<IModel> readModel(URL url) 
+	public List<IModel> readModel(URL url)
 	{
 		JAXBContext context;
-		
+
 		List<IModel> sphModels = new ArrayList<IModel>();
-		try 
+		try
 		{
 			context = JAXBContext.newInstance(SPHModel.class);
 			Unmarshaller um = context.createUnmarshaller();
 			SPHModelX sphModel = (SPHModelX) um.unmarshal(url);
 			sphModels.add(sphModel);
-		} 
-		catch (JAXBException e1) 
+		}
+		catch (JAXBException e1)
 		{
 			e1.printStackTrace();
 		}
 		return sphModels;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openworm.simulationengine.core.model.IModelInterpreter#getSceneFromModel(java.util.List)
+	 */
+	public Scene getSceneFromModel(List<IModel> model)
+	{
+		Scene scene = new Scene();
+		for (IModel m : model)
+		{
+			Entity e = new Entity();
+			scene.getEntities().add(e);
+			SPHModelX sphModel = (SPHModelX) m;
+			for (SPHParticle p : sphModel.getParticles())
+			{
+				e.getGeometries().add(getParticleGeometry(p));
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param sphp
+	 * @return
+	 */
+	private AGeometry getParticleGeometry(SPHParticle sphp)
+	{
+		Particle p = new Particle();
+		Point point=new Point();
+		point.setX((double) sphp.getPositionVector().getX());
+		point.setY((double) sphp.getPositionVector().getY());
+		point.setZ((double) sphp.getPositionVector().getZ());
+		p.setPosition(point);
+		return p;
 	}
 
 }
