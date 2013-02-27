9d748d1208cb3fdc474440e2b2fb5d20b29f66cf
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-02-27 11:59:42 +0000

    Added resource

--- src/main/java/org/openworm/simulationengine/model/sph/common/SPHConstants.java
+++ src/main/java/org/openworm/simulationengine/model/sph/common/SPHConstants.java
@@ -4,11 +4,11 @@ public class SPHConstants {
 	public static final int NEIGHBOR_COUNT = 32;
 	
 	public static final float XMIN = 0;
-	public static final float XMAX = 11.69f; //120.24f;
+	public static final float XMAX = 120.24f;
 	public static final float YMIN = 0;
-	public static final float YMAX = 11.69f; //80.16f;
+	public static final float YMAX = 80.16f;
 	public static final float ZMIN = 0;
-	public static final float ZMAX = 11.69f; //180.36f;
+	public static final float ZMAX = 180.36f;
 	
 	public static final float LIQUID_TYPE = 1.1f;
 	public static final float ELASTIC_TYPE = 2.1f;
