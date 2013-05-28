9e00745a5fba822c8557db03c2e007d6775e1574
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-05-28 14:13:07 +0100

    Changed StateSet to StateTree. Added visitors as a way to operate with the tree.

--- src/main/java/org/geppetto/model/sph/services/SPHModelInterpreterService.java
+++ src/main/java/org/geppetto/model/sph/services/SPHModelInterpreterService.java
@@ -13,16 +13,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geppetto.core.model.IModel;
 import org.geppetto.core.model.IModelInterpreter;
-import org.geppetto.core.model.StateSet;
-import org.geppetto.core.model.values.FloatValue;
-import org.geppetto.core.visualisation.model.AGeometry;
-import org.geppetto.core.visualisation.model.Entity;
-import org.geppetto.core.visualisation.model.Particle;
-import org.geppetto.core.visualisation.model.Point;
+import org.geppetto.core.model.state.StateTreeRoot;
 import org.geppetto.core.visualisation.model.Scene;
 import org.geppetto.model.sph.SPHModel;
 import org.geppetto.model.sph.SPHParticle;
-import org.geppetto.model.sph.common.SPHConstants;
 import org.geppetto.model.sph.x.SPHModelX;
 import org.geppetto.model.sph.x.SPHParticleX;
 import org.springframework.stereotype.Service;
@@ -70,60 +64,14 @@ public class SPHModelInterpreterService implements IModelInterpreter
 	 * 
 	 * @see org.geppetto.core.model.IModelInterpreter#getSceneFromModel(java.util.List)
 	 */
-	public Scene getSceneFromModel(IModel model, StateSet stateSet)
+	public Scene getSceneFromModel(IModel model, StateTreeRoot stateTree)
 	{
 		long starttime = System.currentTimeMillis();
-		Scene scene = new Scene();
-
-		Entity liquidEntity = new Entity();
-		Entity boundaryEntity = new Entity();
-		Entity elasticEntity = new Entity();
-
-		scene.getEntities().add(liquidEntity);
-		scene.getEntities().add(boundaryEntity);
-		scene.getEntities().add(elasticEntity);
-
-		SPHModelX sphModel = (SPHModelX) model;
-		liquidEntity.setId("LIQUID_" + sphModel.getId());
-		boundaryEntity.setId("BOUNDARY_" + sphModel.getId());
-		elasticEntity.setId("ELASTIC_" + sphModel.getId());
-
-		for(int i=0;i<sphModel.getParticles().size();i++)
-		{
-			Float type = ((FloatValue)stateSet.getLastValueFor(getPropertyPath(i,"pos","p"))).getAsFloat();
-			if(type.equals(SPHConstants.LIQUID_TYPE))
-			{
-				liquidEntity.getGeometries().add(getParticleGeometry(i,stateSet));
-			}
-			else if(type.equals(SPHConstants.ELASTIC_TYPE))
-			{
-				elasticEntity.getGeometries().add(getParticleGeometry(i,stateSet));
-			}
-			else if(type.equals(SPHConstants.BOUNDARY_TYPE))
-			{
-				boundaryEntity.getGeometries().add(getParticleGeometry(i,stateSet));
-			}
-		}
-
+		logger.info("SPH Model to scene conversion starting...");
+		CreateSPHSceneVisitor createSceneVisitor=new CreateSPHSceneVisitor(model.getId());
+		stateTree.apply(createSceneVisitor);
 		logger.info("Model to scene conversion end, took: " + (System.currentTimeMillis() - starttime) + "ms");
-		return scene;
-	}
-
-	/**
-	 * @param sphp
-	 * @return
-	 */
-	private AGeometry getParticleGeometry(int i, StateSet stateSet)
-	{
-		Particle p = new Particle();
-		Point point = new Point();
-		//TODO Workaround: Implement using visitors over values
-		point.setX(((FloatValue)stateSet.getLastValueFor(getPropertyPath(i,"pos","x"))).getAsDouble());
-		point.setY(((FloatValue)stateSet.getLastValueFor(getPropertyPath(i,"pos","y"))).getAsDouble());
-		point.setZ(((FloatValue)stateSet.getLastValueFor(getPropertyPath(i,"pos","z"))).getAsDouble());
-		p.setPosition(point);
-		p.setId(getParticleId(i));
-		return p;
+		return createSceneVisitor.getScene();
 	}
 
 	public static String getPropertyPath(int index, String vector, String property)
