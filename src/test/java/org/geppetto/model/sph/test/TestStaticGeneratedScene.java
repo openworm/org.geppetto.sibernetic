943f3e5de5fe1ef39486dfc6a26f416823faae89
johnidol <giovanni.idili@gmail.com>
2013-06-21 14:21:19 +0100

    added license and refactored test class name

--- src/test/java/org/geppetto/model/sph/test/TestStaticGeneratedScene.java
+++ src/test/java/org/geppetto/model/sph/test/TestStaticGeneratedScene.java
@@ -1,3 +1,36 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011, 2013 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+
 package org.geppetto.model.sph.test;
 
 import java.io.File;
@@ -20,14 +53,14 @@ import org.geppetto.model.sph.x.SPHModelX;
 import org.geppetto.model.sph.x.SPHParticleX;
 import org.junit.Test;
 
-public class TestSceneGeneration {
+public class TestStaticGeneratedScene {
 
 	@Test
-	public void TestSceneGeneration_ElasticScene() throws Exception {
+	public void Test_ElasticScene_InitialConditions() throws Exception {
 		// 1. load reference initial conditions from C++ exported scene
-		String positionString = readFile(TestSceneGeneration.class.getResource("/elastic_position_log_0.txt").getPath());
-		String velocityString = readFile(TestSceneGeneration.class.getResource("/elastic_velocity_log_0.txt").getPath());
-		String connectionsString = readFile(TestSceneGeneration.class.getResource("/elastic_connections_log_0.txt").getPath());
+		String positionString = readFile(TestStaticGeneratedScene.class.getResource("/elastic_position_log_0.txt").getPath());
+		String velocityString = readFile(TestStaticGeneratedScene.class.getResource("/elastic_velocity_log_0.txt").getPath());
+		String connectionsString = readFile(TestStaticGeneratedScene.class.getResource("/elastic_connections_log_0.txt").getPath());
 		String[] positionLines = positionString.split(System.getProperty("line.separator"));
 		String[] velocityLines = velocityString.split(System.getProperty("line.separator"));
 		String[] connectionLines = connectionsString.split(System.getProperty("line.separator"));
